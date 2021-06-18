9d13c58ec5a95a486165c14e42122c93c28026ef
Ali-Doggaz <charfedine611@gmail.com>
2021-06-18 16:37:10 +0100

    Added student class+Mvn dependencies

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,80 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    private Long Id;
+    private String Name;
+    private Integer Age;
+    private LocalDate DOB;
+    private String Email;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, Integer age, LocalDate DOB, String email) {
+        Id = id;
+        Name = name;
+        Age = age;
+        this.DOB = DOB;
+        Email = email;
+    }
+
+    public Student(String name, Integer age, LocalDate DOB, String email) {
+        Name = name;
+        Age = age;
+        this.DOB = DOB;
+        Email = email;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Integer getAge() {
+        return Age;
+    }
+
+    public void setAge(Integer age) {
+        Age = age;
+    }
+
+    public LocalDate getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(LocalDate DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "Id=" + Id +
+                ", Name='" + Name + '\'' +
+                ", Age=" + Age +
+                ", DOB=" + DOB +
+                ", Email='" + Email + '\'' +
+                '}';
+    }
+}
