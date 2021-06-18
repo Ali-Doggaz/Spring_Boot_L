789547832be4b701e6d02b4d916241213720ec85
Ali-Doggaz <charfedine611@gmail.com>
2021-06-18 18:49:06 +0100

    Linked DB to service

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -8,8 +9,16 @@ import java.util.List;
 
 @Service // We can use @Component instead, but @Service shows that this is a service layer. ONLY FOR READABILITY.
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(new Student(1L,"Ali",20, LocalDate.of(2000, Month.JANUARY, 5),"ali@gmail.com"));
+        return studentRepository.findAll();
     }
 }
 
